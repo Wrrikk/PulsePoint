5776043763314059bfab9e81e71bb7d51c0b35a8
Wrrikk <royrik23@gmail.com>
2023-12-09 21:11:01 +0530

    Added Query, Details and Create Handlers

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Application.Activities;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -13,13 +14,14 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-builder.Services.AddCors(opt => {
+builder.Services.AddCors(opt => 
+{
     opt.AddPolicy("CorsPolicy", policy =>
     {
         policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
     });
 });
-
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(List.Handler).Assembly));
 
 var app = builder.Build();
 
