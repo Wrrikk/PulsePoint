2b3e54cd41a4bf7a04869aa1d27a67f7db53297f
Wrrikk <royrik23@gmail.com>
2023-12-22 20:08:10 +0530

    Created an Auth Policy

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -8,6 +8,11 @@ namespace API.Services
 {
     public class TokenService
     {
+        private readonly IConfiguration _config;
+        public TokenService(IConfiguration config)
+        {
+            _config = config;
+        }
         public string CreateToken(AppUser user)
         {
             var claims = new List<Claim>
@@ -17,7 +22,7 @@ namespace API.Services
                 new Claim(ClaimTypes.Email, user.Email),
             };
 
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("unx3nCK3HN8kAszH5LMAxTcVKV8T26tMqZ8xRbbLjqxUM6jpUAUV8fKqchcHTK8u"));
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"]));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
 
             var tokenDescriptor = new SecurityTokenDescriptor
@@ -32,6 +37,6 @@ namespace API.Services
             var token = tokenHandler.CreateToken(tokenDescriptor);
 
             return tokenHandler.WriteToken(token);
-        } 
+        }
     }
 }
